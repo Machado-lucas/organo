d8cf201ea28c1e9bad672bd4a5a96273a41f971f
Lucas Machado <51510946+Machado-lucas@users.noreply.github.com>
2023-05-19 16:22:03 -0300

    primeiro commit pra continuar depois

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,19 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+    let valor = ''
+
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto
